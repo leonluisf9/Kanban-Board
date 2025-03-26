b46b329220cffa0eb287731dc72a4e92276482bc
Maverick <leonluisf9@outlook.com>
2025-03-25 22:33:52 -0500

    add login

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,54 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+// import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
+
+interface UserToken {
+  name: string;
+  exp: number;
+}
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode(this.getToken() || '');
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<UserToken>(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } 
+      
+      return false;
+    } catch (err) {
+      return false;
+    }
   }
 
-  getToken(): string {
+  getToken() {
     // TODO: return the token
+    return localStorage.getItem('id_token');
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
